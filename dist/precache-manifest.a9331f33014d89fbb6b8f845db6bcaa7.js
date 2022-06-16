cf5ef6ca9ba39375925034271c7a7c6eb0d636dc
omar-momen <omarelzhey236@gmail.com>
2022-06-16 13:08:00 +0200

    LOLOLO

---
+++ dist/precache-manifest.a9331f33014d89fbb6b8f845db6bcaa7.js
@@ -0,0 +1,318 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dac175bd52f493cd7510768c3e3d49cc",
+    "url": "/all.css"
+  },
+  {
+    "revision": "bf7800782cfcc225c49b",
+    "url": "/css/app.cb8efdb5.css"
+  },
+  {
+    "revision": "386d9f1c46befab3b120",
+    "url": "/css/chunk-vendors.4480fde8.css"
+  },
+  {
+    "revision": "79bb19e04937c19974260fcb4128270e",
+    "url": "/fonts/Tajawal-Bold.79bb19e0.ttf"
+  },
+  {
+    "revision": "fbca61ce5f0321ab500bdbb168b775b0",
+    "url": "/fonts/Tajawal-Light.fbca61ce.ttf"
+  },
+  {
+    "revision": "2bfe3ee2145f6755e4b5960310daee03",
+    "url": "/fonts/Tajawal-Medium.2bfe3ee2.ttf"
+  },
+  {
+    "revision": "6b10b55e4df612910047b7e5d596a9d5",
+    "url": "/fonts/Tajawal-Regular.6b10b55e.ttf"
+  },
+  {
+    "revision": "1a575a4138e5f366474f0e7c5bd614a5",
+    "url": "/fonts/fa-brands-400.1a575a41.woff"
+  },
+  {
+    "revision": "513aa607d398efaccc559916c3431403",
+    "url": "/fonts/fa-brands-400.513aa607.ttf"
+  },
+  {
+    "revision": "592643a83b8541edc52063d84c468700",
+    "url": "/fonts/fa-brands-400.592643a8.eot"
+  },
+  {
+    "revision": "ed311c7a0ade9a75bb3ebf5a7670f31d",
+    "url": "/fonts/fa-brands-400.ed311c7a.woff2"
+  },
+  {
+    "revision": "766913e6c0088ab8c9f73e18b4127bc4",
+    "url": "/fonts/fa-regular-400.766913e6.ttf"
+  },
+  {
+    "revision": "b0e2db3b634d1bc3928e127458d993d8",
+    "url": "/fonts/fa-regular-400.b0e2db3b.eot"
+  },
+  {
+    "revision": "b91d376b8d7646d671cd820950d5f7f1",
+    "url": "/fonts/fa-regular-400.b91d376b.woff2"
+  },
+  {
+    "revision": "d1d7e3b4c219fde0f7376c6facfd7149",
+    "url": "/fonts/fa-regular-400.d1d7e3b4.woff"
+  },
+  {
+    "revision": "0c6bfc668a72935760178f91327aed3a",
+    "url": "/fonts/fa-solid-900.0c6bfc66.eot"
+  },
+  {
+    "revision": "b9625119ce4300f0ef890a8f3234c773",
+    "url": "/fonts/fa-solid-900.b9625119.ttf"
+  },
+  {
+    "revision": "d745348d289b149026921f197929a893",
+    "url": "/fonts/fa-solid-900.d745348d.woff"
+  },
+  {
+    "revision": "d824df7eb2e268626a2dd9a6a741ac4e",
+    "url": "/fonts/fa-solid-900.d824df7e.woff2"
+  },
+  {
+    "revision": "69a205016bafb43bf9272ccdf7a0de35",
+    "url": "/img/about_section_image.69a20501.png"
+  },
+  {
+    "revision": "03482cf9d6c7631afadcb2480300d7d0",
+    "url": "/img/arabic.03482cf9.svg"
+  },
+  {
+    "revision": "c964f3798ece695b2bde80d53f7ca496",
+    "url": "/img/commerce .c964f379.svg"
+  },
+  {
+    "revision": "87cd0523c98c8c3b0c862ff4e9e6f544",
+    "url": "/img/contact.87cd0523.jpg"
+  },
+  {
+    "revision": "33e4df4ff51f8a1892fb3a624c7bebd4",
+    "url": "/img/contact_bg_shape.33e4df4f.svg"
+  },
+  {
+    "revision": "b4c21de29494e60d86ac43944a24edf0",
+    "url": "/img/english.b4c21de2.svg"
+  },
+  {
+    "revision": "1d5619cd804367cefe6da2d79289218a",
+    "url": "/img/fa-brands-400.1d5619cd.svg"
+  },
+  {
+    "revision": "c5d109be8edd3de0f60eb472bd9ef691",
+    "url": "/img/fa-regular-400.c5d109be.svg"
+  },
+  {
+    "revision": "37bc7099f6f1ba80236164f22e905837",
+    "url": "/img/fa-solid-900.37bc7099.svg"
+  },
+  {
+    "revision": "71d5275a217c5aed238611692cedd6e3",
+    "url": "/img/footer_background.71d5275a.png"
+  },
+  {
+    "revision": "1047b146e91ad3cdafbcfdfabd33eff4",
+    "url": "/img/frensh.1047b146.svg"
+  },
+  {
+    "revision": "3b87cb9f5df35f00dd846b43b98628d3",
+    "url": "/img/german.3b87cb9f.svg"
+  },
+  {
+    "revision": "eead021676372fbfd29779dce0d941b5",
+    "url": "/img/hero_section_shape_1.eead0216.svg"
+  },
+  {
+    "revision": "684bda134af090e074d6a2f188c9beed",
+    "url": "/img/hero_section_shape_2.684bda13.svg"
+  },
+  {
+    "revision": "dfb940dc2b027f393139534df1145059",
+    "url": "/img/hindi.dfb940dc.svg"
+  },
+  {
+    "revision": "3b4cdcc8636c1724f5e85002de5a4343",
+    "url": "/img/it.3b4cdcc8.svg"
+  },
+  {
+    "revision": "bd47fd7a3bd7e1948ae1dd6aabce653c",
+    "url": "/img/italian.bd47fd7a.svg"
+  },
+  {
+    "revision": "cbb8871a3ea667ded0aeae92d00ec14d",
+    "url": "/img/jabanese.cbb8871a.svg"
+  },
+  {
+    "revision": "d0d0f86de4e190f70b498955b5953e8a",
+    "url": "/img/korean.d0d0f86d.svg"
+  },
+  {
+    "revision": "1d5e2d8722587ebe2614b06bf6f2c605",
+    "url": "/img/legal.1d5e2d87.svg"
+  },
+  {
+    "revision": "090a33bf1c139b85d6bfa266fcde7ff4",
+    "url": "/img/logo.090a33bf.svg"
+  },
+  {
+    "revision": "090a33bf1c139b85d6bfa266fcde7ff4",
+    "url": "/img/logo_light.090a33bf.svg"
+  },
+  {
+    "revision": "479e1a5bd82ad5e94be5a503da67c8d7",
+    "url": "/img/medical.479e1a5b.svg"
+  },
+  {
+    "revision": "f150cfeac0a63b0976ff932526c63241",
+    "url": "/img/mouse.f150cfea.svg"
+  },
+  {
+    "revision": "dfdc449a1cf0e3e79b3aad9652ec59b4",
+    "url": "/img/russian.dfdc449a.svg"
+  },
+  {
+    "revision": "a7ff04edd4f1c2f87e42b0cc33199667",
+    "url": "/img/spanish.a7ff04ed.svg"
+  },
+  {
+    "revision": "cd98834355d4c5e3d20e6c9346f7848d",
+    "url": "/img/start_now_image.cd988343.svg"
+  },
+  {
+    "revision": "5bc940815c45ab380138585d70539355",
+    "url": "/img/swedish.5bc94081.svg"
+  },
+  {
+    "revision": "5c8d8c47ac3f9622e118b0d2990cafc0",
+    "url": "/img/transcription.5c8d8c47.svg"
+  },
+  {
+    "revision": "7742e8e9b2358ecebe7ee420eb893243",
+    "url": "/img/turkesh.7742e8e9.svg"
+  },
+  {
+    "revision": "ba58b26ab646e0f5d1fbf78e33079152",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bf7800782cfcc225c49b",
+    "url": "/js/app.ac352f3b.js"
+  },
+  {
+    "revision": "386d9f1c46befab3b120",
+    "url": "/js/chunk-vendors.7865d5ec.js"
+  },
+  {
+    "revision": "4efa5f396b0e4d32e4c765c92ce5c3b1",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "05c475fabceebae1f9d40ba6711cb41a",
+    "url": "/webfonts/fa-brands-400.eot"
+  },
+  {
+    "revision": "e2129c0510f0339c77891735feeacc1b",
+    "url": "/webfonts/fa-brands-400.svg"
+  },
+  {
+    "revision": "8ca6e74eddaf8fce3cdcf8647b37cbf1",
+    "url": "/webfonts/fa-brands-400.ttf"
+  },
+  {
+    "revision": "5d875e6a4dcceea77fa7d4b8e1440982",
+    "url": "/webfonts/fa-brands-400.woff"
+  },
+  {
+    "revision": "7c0be8b6640f024b6f4505161bf1bfd4",
+    "url": "/webfonts/fa-brands-400.woff2"
+  },
+  {
+    "revision": "6bd3ad9dbce9c232c66bfaee1e4c374f",
+    "url": "/webfonts/fa-duotone-900.eot"
+  },
+  {
+    "revision": "3d2ab213cff7f2f1f2f26a25dee7a8d3",
+    "url": "/webfonts/fa-duotone-900.svg"
+  },
+  {
+    "revision": "d80e843337fcdf622266a3d6d0168991",
+    "url": "/webfonts/fa-duotone-900.ttf"
+  },
+  {
+    "revision": "7bc112bc25c528dc4bbfec047c69d338",
+    "url": "/webfonts/fa-duotone-900.woff"
+  },
+  {
+    "revision": "c877e45db0614bba8e121510162149fc",
+    "url": "/webfonts/fa-duotone-900.woff2"
+  },
+  {
+    "revision": "a624ea3f4dd832cb54ea41286ded8fc3",
+    "url": "/webfonts/fa-light-300.eot"
+  },
+  {
+    "revision": "b51b241c5d3754e30868d61057beb228",
+    "url": "/webfonts/fa-light-300.svg"
+  },
+  {
+    "revision": "0e0b8b741276d5220c847d0be486609e",
+    "url": "/webfonts/fa-light-300.ttf"
+  },
+  {
+    "revision": "0b4313e036624b29c8f651d0c9a5ecd8",
+    "url": "/webfonts/fa-light-300.woff"
+  },
+  {
+    "revision": "3d8af27a47bbfab857f96dbcf710b087",
+    "url": "/webfonts/fa-light-300.woff2"
+  },
+  {
+    "revision": "fde04f8e6967b818c6fb3338d8b366d3",
+    "url": "/webfonts/fa-regular-400.eot"
+  },
+  {
+    "revision": "2b8a79b1e1968ee628a76f96eb44e1b6",
+    "url": "/webfonts/fa-regular-400.svg"
+  },
+  {
+    "revision": "faa0b37d4fdba1e304982f192c55bbe2",
+    "url": "/webfonts/fa-regular-400.ttf"
+  },
+  {
+    "revision": "4776aa52b579295cf651f6284793dac4",
+    "url": "/webfonts/fa-regular-400.woff"
+  },
+  {
+    "revision": "3e332211899360ab9380df4d50e1c33a",
+    "url": "/webfonts/fa-regular-400.woff2"
+  },
+  {
+    "revision": "6c207a7b79c06c76e915eb8f30e51d8a",
+    "url": "/webfonts/fa-solid-900.eot"
+  },
+  {
+    "revision": "1687b08928dc7cf6a3d1de0612470fcb",
+    "url": "/webfonts/fa-solid-900.svg"
+  },
+  {
+    "revision": "749dd3651c1a97f03faa88723af2371a",
+    "url": "/webfonts/fa-solid-900.ttf"
+  },
+  {
+    "revision": "fb2f453136d024f47f2463d0d2ea2f05",
+    "url": "/webfonts/fa-solid-900.woff"
+  },
+  {
+    "revision": "150120b3d9b448dd116680b66eba9bbe",
+    "url": "/webfonts/fa-solid-900.woff2"
+  }
+]);
